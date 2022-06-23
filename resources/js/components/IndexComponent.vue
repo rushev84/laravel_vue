9888f1d273bcb574c33f545e5cb8075726741885
SergeyRushev <rushev84@yandex.ru>
2022-06-23 10:21:43 +0300

    refactor: create ShowComponent

--- resources/js/components/IndexComponent.vue
+++ resources/js/components/IndexComponent.vue
@@ -13,16 +13,7 @@
             </thead>
             <tbody>
             <template v-for="person in people">
-                <tr :class="isEdit(person.id) ? 'd-none' : ''">
-                    <th scope="row">{{ person.id }}</th>
-                    <td>{{ person.name }}</td>
-                    <td>{{ person.age }}</td>
-                    <td>{{ person.job }}</td>
-                    <td><a href="#" @click.prevent="changeEditPersonId(person.id, person.name, person.age, person.job)"
-                           class="btn btn-success">Edit</a></td>
-                    <td><a href="#" @click.prevent="deletePerson(person.id)"
-                           class="btn btn-danger">Delete</a></td>
-                </tr>
+                <ShowComponent :person="person" :ref="`show_${person.id}`"></ShowComponent>
                 <EditComponent :person="person" :ref="`edit_${person.id}`"></EditComponent>
             </template>
             </tbody>
@@ -32,6 +23,7 @@
 
 <script>
     import EditComponent from "./EditComponent";
+    import ShowComponent from "./ShowComponent";
     export default {
         name: "IndexComponent",
 
@@ -84,7 +76,7 @@
         computed: {},
 
         components: {
-            EditComponent
+            EditComponent, ShowComponent
         }
     }
 </script>
